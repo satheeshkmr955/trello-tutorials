37f11f1ddebe693ae520812acc15940d5e24695e
satheesh <satheeshkmr955@gmail.com>
2023-12-10 13:44:34 +0530

    completed landing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navbar } from "./_components/navbar";
+import { Footer } from "./_components/footer";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const MarketingLayout = (props: Props) => {
+  const { children } = props;
+  return (
+    <div className="h-full bg-slate-100">
+      <Navbar />
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingLayout;
