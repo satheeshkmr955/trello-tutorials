1ef10f38cfd9beabcc40a1ff6773d33fd8d47b6a
satheesh <satheeshkmr955@gmail.com>
2023-12-12 00:08:32 +0530

    completed sidebar

--- app/(platform)/(dashboard)/_component/navbar.tsx
+++ app/(platform)/(dashboard)/_component/navbar.tsx
@@ -4,9 +4,12 @@ import { Plus } from "lucide-react";
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 
+import { MobileSidebar } from "./mobile-sidebar";
+
 export const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center px-4">
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
