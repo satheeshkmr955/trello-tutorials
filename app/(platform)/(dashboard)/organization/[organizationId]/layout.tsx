9d35e9022b5c9caed57933ad1e2463c73718acfc
satheesh <satheeshkmr955@gmail.com>
2023-12-17 12:40:20 +0530

    completed board list

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
+
 import { OrgControl } from "./_component/org-control";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 type Props = {
   children: React.ReactNode;
